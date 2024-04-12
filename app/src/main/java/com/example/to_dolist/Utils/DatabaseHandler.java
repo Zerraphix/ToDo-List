d00b216b7f58ddf1629b91e85bde5ac4c31656f3
Zerraphix <joey.knutsson@rocketmail.com>
2024-04-12 11:43:01 +0200

    Database CRUD

---
+++ app/src/main/java/com/example/to_dolist/Utils/DatabaseHandler.java
@@ -0,0 +1,101 @@
+package com.example.to_dolist.Utils;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.to_dolist.Model.ToDoModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    private static final int VERSION = 1;
+    private static final String NAME = "toDoListDB";
+    private static final String TODO_TABLE = "todo";
+    private static final String ID = "id";
+    private static final String TASK = "task";
+    private static final String ISCOMPLETED = "iscompleted";
+    private static final String CREATE_TODO_TABLE = "CREATE TABLE " + TODO_TABLE + "(" + ID + "," + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                                            + TASK + "TEXT, " + ISCOMPLETED + "INTEGER)";
+    private SQLiteDatabase db;
+
+    private DatabaseHandler(Context context){
+        super(context, NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL(CREATE_TODO_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        // Drop the older tables
+        db.execSQL("DROP TABLE IF EXISTS" + TODO_TABLE);
+
+        // Create tables again
+        onCreate(db);
+    }
+
+    public void openDatabase(){
+        db = this.getWritableDatabase();
+    }
+
+    // CRUD
+
+    // Create
+    public void creatTask(ToDoModel task){
+        ContentValues cv = new ContentValues();
+        cv.put(TASK, task.getTask());
+        cv.put(ISCOMPLETED, false);
+        db.insert(TODO_TABLE, null, cv);
+    }
+
+    // Read
+    public List<ToDoModel> getAllTasks(){
+        List<ToDoModel> taskList = new ArrayList<>();
+        Cursor cur = null;
+        db.beginTransaction();
+        try{
+            cur = db.query(TODO_TABLE, null, null,  null, null, null, null, null);
+            if(cur != null){
+                if(cur.moveToFirst()){
+                    do{
+                        ToDoModel task = new ToDoModel();
+                        task.setId(cur.getInt(cur.getColumnIndex(ID)));
+                        task.setTask(cur.getString(cur.getColumnIndex(TASK)));
+                        task.setCompleted(cur.getInt(cur.getColumnIndex(ISCOMPLETED)));
+                        taskList.add(task);
+                    } while (cur.moveToNext());
+                }
+            }
+        }
+        finally {
+            db.endTransaction();
+            cur.close();
+        }
+        return taskList;
+    }
+
+    // Update
+    public void updateCompleted(int id, boolean isCompleted){
+        ContentValues cv = new ContentValues();
+        cv.put(ISCOMPLETED, isCompleted);
+        db.update(TODO_TABLE, cv, ID + "=?", new String[] {String.valueOf(id)});
+    }
+
+    public void updateTask(int id, String task){
+        ContentValues cv = new ContentValues();
+        cv.put(TASK, task);
+        db.update(TODO_TABLE, cv, ID + "=?", new String[] {String.valueOf(id)});
+    }
+
+    // Delete
+    public  void deleteTask(int id){
+        db.delete(TODO_TABLE, ID + "=?", new String[] {String.valueOf(id)});
+    }
+}
